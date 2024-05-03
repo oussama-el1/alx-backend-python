03da96a9fc9977733275bebd81e1d408b436ee29
oussama-el1 <elhadraoui284@gmail.com>
2024-05-03 23:09:36 +0100

    task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-def add(a:float, b: float) -> float:
+def add(a: float, b: float) -> float:
     """ function take two float and return the sum as float"""
     return a + b
