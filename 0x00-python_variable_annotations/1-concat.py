e1dc1059864f5bee54de134d42352e776b73d284
oussama-el1 <elhadraoui284@gmail.com>
2024-05-03 23:25:47 +0100

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+this is a file for concat type-annotated function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat two string"""
+    return str1 + str2
